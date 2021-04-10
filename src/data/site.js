e2c06552b89a98a71178ec5e17844af83d3be7a8
Stefan Imhoff <stefan@imhoff.name>
2021-04-10 16:07:13 +0200

    chore: have different page title on homepage

--- src/data/site.js
+++ src/data/site.js
@@ -1,5 +1,6 @@
 module.exports = {
   buildTime: new Date(),
+  homepageTitle: 'Front-End Web Developer from Hamburg, Germany',
   owner: 'Stefan Imhoff',
   tagline: 'Front-End Web Developer • Designer • Minimalist • Japanophile',
 };
