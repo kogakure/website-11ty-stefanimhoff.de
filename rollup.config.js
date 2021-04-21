a2927c39baa184c92685b22f824de226c244a997
Stefan Imhoff <stefan@imhoff.name>
2021-04-21 19:18:36 +0200

    use Critical CSS to improve performance

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,29 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
+const cssFiles = [
+  'about',
+  'base',
+  'colors-japan',
+  'cv',
+  'error-404',
+  'haiku',
+  'homepage',
+  'journal',
+  'projects',
+  'sketchnotes',
+];
+
+const criticalCSS = cssFiles.map((file) => ({
+  input: `src/assets/styles/critical/${file}.css`,
+  output: { file: `src/includes/critical/${file}.css` },
+  plugins: [postcss({ config: './postcss.config.js', extract: true })],
+  onwarn(warning, warn) {
+    if (warning.code === 'FILE_NAME_CONFLICT') return;
+    warn(warning);
+  },
+}));
+
 const config = [
   {
     input: 'src/assets/scripts/main.js',
@@ -22,27 +45,36 @@ const config = [
   {
     input: 'src/assets/styles/main.css',
     output: {
-      dir: './',
+      file: 'dist/assets/styles/main.css',
     },
     plugins: [
       postcss({
         config: './postcss.config.js',
-        extract: 'dist/assets/styles/main.css',
+        extract: true,
       }),
     ],
+    onwarn(warning, warn) {
+      if (warning.code === 'FILE_NAME_CONFLICT') return;
+      warn(warning);
+    },
   },
   {
     input: 'src/assets/styles/print.css',
     output: {
-      dir: './',
+      file: 'dist/assets/styles/print.css',
     },
     plugins: [
       postcss({
         config: './postcss.config.js',
-        extract: 'dist/assets/styles/print.css',
+        extract: true,
       }),
     ],
+    onwarn(warning, warn) {
+      if (warning.code === 'FILE_NAME_CONFLICT') return;
+      warn(warning);
+    },
   },
+  ...criticalCSS,
 ];
 
 export default config;
