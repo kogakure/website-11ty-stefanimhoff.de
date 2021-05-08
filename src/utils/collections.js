82ff0daa6e3f2f14f1599f35d969b9759e2518e9
Stefan Imhoff <stefan@imhoff.name>
2021-05-08 11:23:21 +0200

    feat: add hub pages for links and quotes

--- src/utils/collections.js
+++ src/utils/collections.js
@@ -15,4 +15,32 @@ module.exports = {
       .reverse()
       .value();
   },
+  linksByYear: function (collection) {
+    return _.chain(
+      collection
+        .getFilteredByTag('links')
+        .filter(
+          (item) =>
+            !(item.data.draft && process.env.ELEVENTY_ENV == 'production')
+        )
+    )
+      .groupBy((post) => post.date.getFullYear())
+      .toPairs()
+      .reverse()
+      .value();
+  },
+  quotesByYear: function (collection) {
+    return _.chain(
+      collection
+        .getFilteredByTag('quotes')
+        .filter(
+          (item) =>
+            !(item.data.draft && process.env.ELEVENTY_ENV == 'production')
+        )
+    )
+      .groupBy((post) => post.date.getFullYear())
+      .toPairs()
+      .reverse()
+      .value();
+  },
 };
