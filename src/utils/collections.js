a04487bf4db5c1af4a609df7af062cd4c3ddf5bf
Stefan Imhoff <stefan@imhoff.name>
2021-12-19 12:45:25 +0100

    feat: add collection to hide draft posts

--- src/utils/collections.js
+++ src/utils/collections.js
@@ -1,6 +1,10 @@
 const _ = require('lodash');
+const livePosts = (p) => !p.data.draft;
 
 module.exports = {
+  livePosts: function (collection) {
+    return collection.getFilteredByTag('journal').filter(livePosts).reverse();
+  },
   journalByYear: function (collection) {
     return _.chain(
       collection
